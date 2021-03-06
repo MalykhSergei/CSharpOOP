e7b3c5ed34ec44422143eda7032450a50360c982
Sergei Malykh <sergej.malykh@gmail.com>
2021-03-06 21:52:47 +0100

    ArrayListHome

---
+++ ArrayListHomeTask/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListHomeTask
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Utils.ReadFile("1.txt")));
+
+            Console.WriteLine();
+
+            List<int> list = new List<int>() { 1, 2, 3, 4, 4, 4, 4, 4, 5, 5 };
+
+            Utils.RemoveEvenNumbers(list);
+
+            Console.WriteLine(string.Join(", ", list));
+
+            Console.WriteLine(string.Join(", ", Utils.RemoveDuplicateItems(list)));
+        }
+    }
+}
---
+++ ArrayListHomeTask/Utils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ArrayListHomeTask
+{
+    class Utils
+    {
+        public static List<string> ReadFile(string fileName)
+        {
+            List<string> list = new List<string>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string currentLine;
+
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        list.Add(currentLine);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File is not found!");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return list;
+        }
+
+        public static void RemoveEvenNumbers(List<int> list)
+        {
+            list.RemoveAll(e => e % 2 == 0);
+        }
+
+        public static List<T> RemoveDuplicateItems<T>(List<T> list)
+        {
+            List<T> newList = new List<T>();
+
+            foreach (T item in list)
+            {
+                if (!newList.Contains(item))
+                {
+                    newList.Add(item);
+                }
+            }
+
+            return newList;
+        }
+    }
+}
