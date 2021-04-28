7b4a9f80a73ade7fe6f9c22e9c528affab9668b8
Sergei Malykh <sergej.malykh@gmail.com>
2021-04-28 22:19:27 +0200

    Graph

---
+++ GraphTask/App.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace GraphTask
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 =
+            {
+                {0, 1, 0, 0, 0, 0, 0 },
+                {1, 0, 1, 1, 1, 1, 0 },
+                {0, 1, 0, 0, 0, 0, 1 },
+                {0, 1, 0, 0, 0, 0, 0 },
+                {0, 1, 0, 0, 0, 1, 0 },
+                {0, 1, 0, 0, 1, 0, 1 },
+                {0, 0, 1, 0, 0, 1, 0 }
+            };
+
+            int[,] matrix2 = {
+                { 0, 1, 0, 0, 0, 1, 1},
+                { 1, 0, 0, 0, 1, 1, 0},
+                { 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0},
+                { 0, 1, 0, 0, 0, 0, 0},
+                { 1, 1, 0, 0, 0, 0, 0},
+                { 1, 0, 0, 0, 0, 0, 0}
+            };
+
+            Graph graph1 = new Graph(matrix2);
+            Graph graph2 = new Graph(matrix1);
+
+            graph1.PassInWidth(Console.WriteLine);
+
+            Console.WriteLine();
+
+            graph2.PassInDepth(Console.WriteLine);
+        }
+    }
+}
---
+++ GraphTask/Graph.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphTask
+{
+    class Graph
+    {
+        private readonly int[,] graph;
+
+        public Graph(int[,] graph)
+        {
+            this.graph = graph;
+        }
+
+        public void PassInWidth(Action<int> action)
+        {
+            if (graph.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(graph), "Graph is empty");
+            }
+
+            Queue<int> queue = new Queue<int>();
+            bool[] visited = new bool[graph.Length];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (!visited[i])
+                {
+                    queue.Enqueue(i);
+
+                    while (queue.Count != 0)
+                    {
+                        int currentNode = queue.Dequeue();
+
+                        if (!visited[currentNode])
+                        {
+                            visited[currentNode] = true;
+                            action(currentNode);
+
+                            for (int j = 0; j < graph.GetLength(0); j++)
+                            {
+                                if (!visited[j] && graph[currentNode, j] == 1)
+                                {
+                                    queue.Enqueue(j);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void VisitNode(int node, bool[] visited, Action<int> action)
+        {
+            if (!visited[node])
+            {
+                visited[node] = true;
+                action(node);
+
+                for (int i = 0; i < visited.Length; i++)
+                {
+                    if (!visited[i] && graph[node, i] == 1)
+                    {
+                        VisitNode(i, visited, action);
+                    }
+                }
+            }
+        }
+
+        public void PassInDepth(Action<int> action)
+        {
+            if (graph.GetLength(0) == 0)
+            {
+                if (graph.Length == 0)
+                {
+                    throw new ArgumentNullException(nameof(graph), "Graph is empty");
+                }
+            }
+
+            bool[] visited = new bool[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (!visited[i])
+                {
+                    VisitNode(i, visited, action);
+                }
+            }
+        }
+    }
+}
